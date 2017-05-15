6bb1c04c3632df3cf0b55079816172e1404192a6
Ryuhikari <fernandovillab@gmail.com>
2017-05-15 02:32:26 +0200

    Check doctrine and entities

--- src/BackendBundle/Controller/DefaultController.php
+++ src/BackendBundle/Controller/DefaultController.php
@@ -8,6 +8,16 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $user_repo = $em->getRepository("BackendBundle:User");
+        
+        $user = $user_repo->find(1);
+        
+        echo "Wellcome ".$user->getName()." ".$user->getSurname();
+        
+        var_dump($user);
+        die();
+        
         return $this->render('BackendBundle:Default:index.html.twig');
     }
 }
